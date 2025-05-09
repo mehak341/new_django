22ce3636ff5c49e482c11c28aed12158a762dba5
mehak341 <mehaknaaz7272@gmail.com>
2025-05-09 12:07:24 +0530

    python manage.py

---
+++ python manage.py
@@ -0,0 +1,4 @@
+ # To make migrations if this is your first time connecting to a database
+ python manage.py makemigrations
+ #To migrate tables set on your migrations folders
+ python manage.py migrate
